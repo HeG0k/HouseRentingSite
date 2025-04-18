2d4934f9f070713bdd1905458711e144c807b9cd
HeG0k <145152513+HeG0k@users.noreply.github.com>
2025-04-18 20:58:47 +0300

    Создание таблицы пользователей

---
+++ server.py
@@ -0,0 +1,22 @@
+
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+import sqlite3
+from werkzeug.security import generate_password_hash, check_password_hash
+from functools import wraps
+import os
+from werkzeug.utils import secure_filename
+def init_db():
+    conn = sqlite3.connect('users.db')
+    c = conn.cursor()
+
+    # Создание таблицы пользователей
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL,
+            role INTEGER NOT NULL DEFAULT 1
+        )
+    ''')
+    conn.commit()
+    conn.close()
